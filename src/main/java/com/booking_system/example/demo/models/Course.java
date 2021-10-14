021cc1f5c6fa929bc6aaccc02e506498cab85b29
mitkousa <uk.dimitar@gmail.com>
2021-10-14 10:22:03 +0100

    models done

--- src/main/java/com/booking_system/example/demo/models/Course.java
+++ src/main/java/com/booking_system/example/demo/models/Course.java
@@ -1,4 +1,57 @@
 package com.booking_system.example.demo.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private String date;
+    private String town;
+    private int starRating;
+    private List<Booking> bookings;
+
+    public Course(String date, String town, int starRating) {
+        this.date = date;
+        this.town = town;
+        this.starRating = starRating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public  void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
 }
--- src/main/java/com/booking_system/example/demo/models/Customer.java
+++ src/main/java/com/booking_system/example/demo/models/Customer.java
@@ -1,4 +1,57 @@
 package com.booking_system.example.demo.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private String name;
+    private String town;
+    private int age;
+    private List<Booking> bookings;
+
+    public Customer(String name, String town, int age) {
+        this.name = name;
+        this.town = town;
+        this.age = age;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Customer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking){
+        this.bookings.add(booking);
+    }
 }
